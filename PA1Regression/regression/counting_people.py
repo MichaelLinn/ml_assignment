e083c5569f73aef4a6bf7805dd107a6d5e9bca3e
LIN Jiecong <jieconlin3@LINs-MacBook-Pro.local>
2017-10-08 20:02:42 +0800

    add counting_people

--- PA1Regression/regression/counting_people.py
+++ PA1Regression/regression/counting_people.py
@@ -7,3 +7,202 @@
 import os
 import numpy as np
 import pandas as pd
+from polynomial_function import Regression
+import matplotlib.pylab as plt
+
+class counting_people:
+
+    def __init__(self):
+        self.load_data()
+
+
+    def load_data(self):
+        os.getcwd()
+        os.chdir(os.pardir)
+        os.chdir("data")
+        self.testX = np.loadtxt("count_data_testx.txt")
+        self.testY = np.loadtxt("count_data_testy.txt")
+        # print self.testX.shape
+        self.trainX = np.loadtxt("count_data_trainx.txt")
+        self.trainY = np.loadtxt("count_data_trainy.txt")
+        # print self.trainX.shape
+
+
+    def genOriginalPhi(self, inputX=None):
+
+        # inputX is transposed
+        if inputX is None:
+            inputX = self.trainX
+
+        matPhi = []
+        for x in inputX:
+            col = []
+            for i in x:
+                col.append(i)
+            matPhi.append(col)
+        matPhi = (np.asmatrix(matPhi))
+        # print matPhi
+        # print self.trainX.shape
+        # print "Phi:" + str(matPhi.shape)
+        return matPhi
+
+    def gen2ndPolyPhi(self, inputX=None):
+
+        # inputX is transposed
+        if inputX is None:
+            inputX = self.trainX
+
+        matPhi_1 = self.genOriginalPhi(inputX)
+        matPhi_2 = []
+        for x in inputX:
+            col = []
+            for i in x:
+                col.append(i**2)
+            matPhi_2.append(col)
+        matPhi_2 = (np.asmatrix(matPhi_2))
+        matPhi = np.row_stack((matPhi_1, matPhi_2))
+        # print matPhi
+        # print self.trainX.shape
+        # print "Phi:" + str(matPhi.shape)
+        return matPhi
+
+    def predict(self, theta_, inputX, phi_type=1):
+
+        if phi_type == 1:
+            theta_ = np.array(theta_)
+            theta__ = np.mat(theta_.flatten()).T  # change ND array into 1D array
+            phi_ = self.genOriginalPhi(inputX=inputX.T)
+            predictY = phi_.T * theta__
+            return predictY
+
+        if phi_type == 2:
+            theta_ = np.array(theta_)
+            theta_ = np.mat(theta_.flatten()).T  # change ND array into 1D array
+            phi_ = self.gen2ndPolyPhi(inputX=inputX.T)
+            predictY = phi_.T * theta_
+            return predictY
+
+
+    def lsRegression(self):
+        reg = Regression()
+        reg.vecX = self.trainX
+        reg.vecY = self.trainY.T
+        theta_ = reg.leastSquaresReg(matPhi=self.gen2ndPolyPhi())
+        # print theta_.shape
+        predictY = self.predict(theta_, self.testX.T, phi_type=2)
+        # print self.testX.shape
+
+        xAxis = np.arange(len(self.testX.T))
+        # print xAxis.shape
+        fig = plt.figure()
+        plt.scatter(xAxis, predictY, c="red", s=4, marker=".", label="Predicted Value")
+        plt.scatter(xAxis, self.testY, s=4, marker="^", label="True Value", facecolors="none", c="blue")
+        mse, mae = reg.countErr(predictY, self.testY)
+        err_str = "MSE=" + str(mse) + "\nMAE=" + str(mae)
+        plt.text(200, -12, err_str)
+        plt.title("Least Square Regression ($\phi(x)=[x;x^2]$)")
+        plt.legend(loc="best")
+        fig.savefig("/Users/jieconlin3/Desktop/ls_cp_2phi.eps", format='eps', dpi=1000)
+        # plt.show()
+
+    def rlsRegression(self):
+        reg = Regression()
+        reg.vecX = self.trainX.T
+        reg.vecY = self.trainY.T
+        theta_ = reg.regularizedLSReg(matPhi=self.gen2ndPolyPhi())
+        # print theta_.shape
+        predictY = self.predict(theta_, self.testX.T, phi_type=2)
+        # print self.testX.shape
+
+        xAxis = np.arange(len(self.testX.T))
+        # print xAxis.shape
+        fig = plt.figure()
+        plt.scatter(xAxis, predictY, c="red", s=4, marker=".", label="Predicted Value")
+        plt.scatter(xAxis, self.testY, c="blue", s=4, marker="^", label="True Value")
+        mse, mae = reg.countErr(predictY, self.testY)
+        err_str = "MSE=" + str(mse) + "\nMAE=" + str(mae)
+        plt.text(200, -12, err_str)
+        plt.title("Regularized Least Square Regression ($\phi(x)=[x;x^2]$)")
+        plt.legend(loc="best")
+        fig.savefig("/Users/jieconlin3/Desktop/rls_cp_2phi.eps", format='eps', dpi=1000)
+        # plt.show()
+
+
+    def robustRegression(self):
+        reg = Regression()
+        reg.vecX = self.trainX.T
+        reg.vecY = self.trainY.T
+        theta_ = reg.robustReg(matPhi=self.gen2ndPolyPhi())
+        # print theta_.shape
+        predictY = self.predict(theta_, self.testX.T, phi_type=2)
+        # print self.testX.shape
+
+        xAxis = np.arange(len(self.testX.T))
+        # print xAxis.shape
+        fig = plt.figure()
+        plt.scatter(xAxis, predictY, c="red", s=4, marker=".", label="Predicted Value")
+        plt.scatter(xAxis, self.testY, c="blue", s=4, marker="^", label="True Value")
+        mse, mae = reg.countErr(predictY, self.testY)
+        err_str = "MSE=" + str(mse) + "\nMAE=" + str(mae)
+        plt.text(200, -12, err_str)
+        plt.title("Robust Regression ($\phi(x)=[x;x^2]$)")
+        plt.legend(loc="best")
+        fig.savefig("/Users/jieconlin3/Desktop/rr_cp.eps", format='eps', dpi=1000)
+        # plt.show()
+
+    def lassoRegression(self):
+
+        reg = Regression()
+        reg.vecX = self.trainX.T
+        reg.vecY = self.trainY.T
+        theta_ = reg.lassoReg(matPhi=self.gen2ndPolyPhi())
+        # print theta_.shape
+        predictY = self.predict(theta_, self.testX.T, phi_type=2)
+        # print self.testX.shape
+
+        xAxis = np.arange(len(self.testX.T))
+        # print xAxis.shape
+        fig = plt.figure()
+        plt.scatter(xAxis, predictY, c="red", s=4, marker=".", label="Predicted Value")
+        plt.scatter(xAxis, self.testY, c="blue", s=4, marker="^", label="True Value")
+
+        mse, mae = reg.countErr(predictY, self.testY)
+        err_str = "MSE=" + str(mse) + "\nMAE=" + str(mae)
+        plt.text(200, -12, err_str)
+        plt.title("Lasso Regression ($\phi(x)=[x;x^2]$)")
+        plt.legend(loc="best")
+        fig.savefig("/Users/jieconlin3/Desktop/lasso_cp_2phi.eps", format='eps', dpi=1000)
+        # plt.show()
+
+    def baysesianRegression(self):
+
+        reg = Regression()
+        reg.vecX = self.trainX
+        reg.vecY = self.trainY.T
+
+        mu_theta, sigma_theta = reg.baysesianReg(matPhi=self.gen2ndPolyPhi())
+        phi_s = self.gen2ndPolyPhi(inputX=self.testX)
+
+        predictY = phi_s.T * mu_theta
+        xAxis = np.arange(len(self.testX.T))
+
+        fig = plt.figure()
+        plt.scatter(xAxis, predictY, c="red", s=4, marker=".", label="Predicted Value")
+        plt.scatter(xAxis, self.testY, c="blue", s=4, marker="^", label="True Value")
+
+        mse, mae = reg.countErr(predictY, self.testY)
+        err_str = "MSE=" + str(mse) + "\nMAE=" + str(mae)
+        plt.text(200, -12, err_str)
+        plt.title("Bayessian Regression ($\phi(x)=[x;x^2]$)")
+        plt.legend(loc="best")
+        fig.savefig("/Users/jieconlin3/Desktop/bayessian_cp_2phi.eps", format='eps', dpi=1000)
+
+
+
+
+t = counting_people()
+t.lsRegression()
+t.rlsRegression()
+t.robustRegression()
+t.lassoRegression()
+t.baysesianRegression()
--- PA1Regression/regression/polynomial_function.py
+++ PA1Regression/regression/polynomial_function.py
@@ -4,20 +4,18 @@
 # @File    : polynomial_function.py
 # @Software: PyCharm Community Edition
 
-import pandas as pd
 import numpy as np
 import os
-from pylab import *
 import matplotlib.pylab as plt
-from scipy.optimize import linprog
-from scipy import stats
 from cvxopt import matrix, solvers
+import random
 
 
 class Regression:
 
     polyOrder = 5
-
+    rr_error = []
+    ls_error =[]
 
     def __init__(self):
         plt.figure(1)
@@ -33,31 +31,32 @@ class Regression:
 
         self.polyx = np.loadtxt("polydata_data_polyx.txt")
         self.polyy = np.loadtxt("polydata_data_polyy.txt")
-        # scatter(self.vecX, self.vecY, marker="o", s=9, alpha=0.5, c="black")
-        # show()
+
 
     # least squares regression (LS)
-    def leastSquaresReg(self):
-        matPhi = self.genPhi()
+    def leastSquaresReg(self, matPhi = None):
+        if matPhi is None:
+            matPhi = self.genPhi()
         theta_LS = (matPhi * matPhi.T).I * matPhi * np.mat(self.vecY).T
         # print theta_LS
-        plt.subplot(321, xlabel="Least Squares")
-        self.plotReg(theta_LS)
+        # plt.subplot(321, xlabel="Least Squares")
+        # self.plotReg(theta_LS)
+        return theta_LS
 
     # regularized LS (RLS)
-    def regularizedLSReg(self):
-        lambda_ = 0.1
-        matPhi = self.genPhi()
+    def regularizedLSReg(self, lambda_=0.1, matPhi=None):
+        # lambda_ = 0.1
+        if matPhi is None:
+            matPhi = self.genPhi()
         matUnit = np.identity((matPhi * matPhi.T).shape[0])
         theta_RLS = (matPhi * matPhi.T + lambda_ * matUnit).I * matPhi * np.mat(self.vecY).T
-        plt.subplot(322, xlabel="Regularized")
-        self.plotReg(theta_RLS)
+        # plt.subplot(322, xlabel="Regularized")
+        # self.plotReg(theta_RLS)
+
+        return theta_RLS
 
 
     def plotReg(self, theta_):
-
-        # print theta_
-        # print "-------------------"
 
         testX = np.linspace(-2.0, 2.0, 200, endpoint=True)
 
@@ -68,61 +67,91 @@ class Regression:
 
         predictY = p(self.polyx)
 
-        error = self.countMeanSqureErr(predictY, self.polyy)
+        error = self.countErr(predictY, self.polyy)
         text = "Error=" + str(error)
-        plt.text(-0.5,25,text)
+        plt.text(-0.5, 25, text)
 
-        plt.plot(testX, resY)
-        plt.scatter(self.vecX, self.vecY, marker="o", s=9, alpha=0.5, c="black")
+        plt.plot(testX, resY, label="Regression")
+        plt.scatter(self.vecX, self.vecY, marker="o", s=9, alpha=0.5, c="black", label="Sample point")
+        plt.scatter(self.polyx, self.polyy, marker="o", c="red",s=7, label="True Point")
+        plt.legend(loc="best")
+
+    def predict(self, theta_, inputX):
+
+        theta_ = np.array(theta_[::-1])  # reverse
+        theta_ = theta_.flatten()  # change ND array into 1D array
+        p = np.poly1d(theta_)
+        predictY = p(inputX)
+        return predictY
 
 
+    def genPhi(self, inputX=None):
 
-    def genPhi(self):
-
+        if inputX is None:
+            inputX = self.vecX
+        theta_len = self.polyOrder + 1
         f = lambda x, n: x ** n
         matPhi = []
-        for x in self.vecX:
+        for x in inputX:
             col = []
-            for i in range(self.polyOrder + 1):
+            for i in range(theta_len):
                 col.append(f(x, i))
             matPhi.append(col)
         matPhi = (np.asmatrix(matPhi)).T
         # print matPhi
+        self.matPhi = matPhi
         return matPhi
 
-    def robustReg(self):
+    def robustReg(self, matPhi=None):
+
+
+        if matPhi is None:
+            matPhi = self.genPhi()
+            theta_len = self.polyOrder + 1
+        else:
+            theta_len = len(matPhi)
 
         vecF = []
-        for i in range(self.polyOrder + 1):
+        for i in range(theta_len):
             vecF.append(0.0)
         for i in range(len(self.vecX)):
             vecF.append(1.0)
         vecF = np.array(vecF)
 
-        matPhi = self.genPhi()
+
         matIdentity = np.identity(len(self.vecX))
+
         matA_1 = np.column_stack((-matPhi.T, -matIdentity))
         matA_2 = np.column_stack((matPhi.T, -matIdentity))
         matA = np.row_stack((matA_1, matA_2))
         vec_b = np.concatenate((-self.vecY, self.vecY))
+
         # objectiveFunc = lambda X: vecF.T * X
         # consFunc = lambda X: -(matA * X - vec_b)
         # res = linprog(vecF, A_ub=matA, b_ub=vec_b)
         # theta_ = res.x[:6]
-        sol = solvers.lp(matrix(vecF), matrix(matA), matrix(vec_b))
-        theta_ =  list(sol['x'][:6])
+        sol = solvers.lp(matrix(vecF), matrix(matA), matrix(vec_b), solver=solvers)
+        theta_ =  list(sol['x'][:theta_len])
         print theta_
 
-        plt.subplot(323, xlabel="Robust")
-        self.plotReg(theta_)
+        # plt.subplot(323, xlabel="Robust")
+        # self.plotReg(theta_)
+
+        return theta_
 
 
-    def baysesianReg(self):
-        alpha_ = 1
+    def baysesianReg(self, alpha_=1.0, matPhi=None):
+
+        if matPhi is None :
+            matPhi = self.genPhi()
+            theta_len = self.polyOrder + 1
+        else:
+            theta_len = len(matPhi)
+
         noise = 5
-        matPhi = self.genPhi()
-        sigma_theta = (1.0/alpha_*np.identity(self.polyOrder+1) + 1.0/ 5 * matPhi*matPhi.T).I
-        mu_theta = 1.0/(noise) * sigma_theta * matPhi * np.mat(self.vecY).T
+
+        sigma_theta = (1.0/alpha_*np.identity(theta_len) + 1.0/ 5 * matPhi * matPhi.T).I
+        mu_theta = 1.0/noise * sigma_theta * matPhi * np.mat(self.vecY).T
 
         # plot regression
         testX = np.linspace(-2.0, 2.0, 200, endpoint=True)
@@ -132,77 +161,173 @@ class Regression:
         varErr = []
         for x in testX:
             phi_s = []
-            for i in range(self.polyOrder + 1):
+            for i in range(theta_len):
                 phi_s.append(f(x,i))
             phi_s = np.mat(phi_s)
             mu_s = phi_s * mu_theta
             sigma_s = phi_s * sigma_theta * phi_s.T
-
-            varErr.append(sqrt(sigma_s.tolist()[0][0]))
+            varErr.append(np.sqrt(sigma_s.tolist()[0][0]))
             resY.append(mu_s.tolist()[0][0])
 
+
+        # predict
         predictY = []
         for x in self.polyx:
             phi_s = []
-            for i in range(self.polyOrder + 1):
+            for i in range(theta_len):
                 phi_s.append(f(x, i))
             phi_s = np.mat(phi_s)
             mu_s = phi_s * mu_theta
             # sigma_s = phi_s * sigma_theta * phi_s.T
             predictY.append(mu_s.tolist()[0][0])
 
+        err, _ = self.countErr(predictY, self.polyy)
 
-        err = self.countMeanSqureErr(predictY, self.polyy)
-        text = "Error=" + str(err)
+        return mu_theta, sigma_theta
 
-        plt.subplot(324, xlabel="Baysesian")
+        # text = "Error=" + str(err)
+
+        # plt.subplot(324, xlabel="Baysesian")
         # plt.plot(testX, resY)
-        plt.errorbar(testX, resY, yerr=varErr, ecolor="red")
-        plt.text(-0.5, 25, text)
+        # plt.errorbar(testX, resY, yerr=varErr, ecolor="red")
+        # plt.text(-0.5, 25, text)
 
-        plt.scatter(self.vecX, self.vecY, marker="o", s=9, alpha=0.5, c="black")
+        # plt.scatter(self.vecX, self.vecY, marker="o", s=9, alpha=0.5, c="black")
 
-    def lassoReg(self):
+
+    def lassoReg(self, matPhi=None):
         lambda_ = 1.0
-        x_dim = 2 * (self.polyOrder + 1)
-        matPhi = self.genPhi()
+        if matPhi is None:
+            matPhi = self.matPhi
+            theta_len = self.polyOrder + 1
+        else:
+            theta_len = len(matPhi)
+
+        x_dim = 2 * theta_len
+
         matH_1 = np.column_stack((matPhi*matPhi.T, -matPhi*matPhi.T))
         matH_2 = np.column_stack((-matPhi*matPhi.T, matPhi*matPhi.T))
         matH = np.row_stack((matH_1, matH_2))
-        print matH
+
         mat_func = lambda_ * np.mat(np.ones(x_dim)).T - np.row_stack((matPhi* (np.mat(self.vecY)).T, -matPhi* (np.mat(self.vecY).T)))
-        print mat_func
+
         matG = np.identity(x_dim)
         vec_h = np.zeros(x_dim)
 
         sol = solvers.qp(matrix(matH), matrix(mat_func), matrix(matG), matrix(vec_h))
         # print sol['x']
 
-        theta_ = list(sol['x'][:6] - sol['x'][6:])
+        theta_ = list(sol['x'][:theta_len] - sol['x'][theta_len:])
         # print theta_
-        plt.subplot(325, xlabel="Lasso")
-        self.plotReg(theta_)
+        # plt.subplot(325, xlabel="Lasso")
+        # self.plotReg(theta_)
+        return theta_
 
-    def countMeanSqureErr(self, preY, trueY):
-        p_y = np.array(preY)
-        t_y = np.array(trueY)
-        err = sum((p_y - t_y) ** 2)/len(preY)
-        return err
+    def countErr(self, preY, trueY):
+
+        p_y = np.array(preY).flatten()
+        t_y = np.array(trueY).flatten()
+        MSE = sum((p_y - t_y) ** 2)/len(preY)
+        MAE = sum(abs(p_y - t_y))/len(preY)
+
+        print MSE
+        print "-----------------"
+        print MAE
+        return MSE, MAE
 
 
-    def adjustTrainingSample(self):
+
+    def genSubSetError(self):
+
+        set_idx = np.arange(len(self.vecX))
+
+        error = {}
+        error['LS'] = []
+        error['RR'] = []
+        error['Lasso'] = []
+        error['RLS'] = []
+        errorX = []
+        for size in range(14, 100, 2):
+            subset_size = size * 0.01 * len(self.vecX)
+            errorX.append(size / 100.0)
+            subset_idx = random.sample(set_idx, int(subset_size))
+            subsetX = []
+            subsetY = []
+            temErr = {}
+            temErr['LS'] = []
+            temErr['RR'] = []
+            temErr['Lasso'] = []
+            temErr['RLS'] = []
+            temError = []
+            for i in range(300):
+                print "---------------" + str(i) + "---------------"
+                for idx in subset_idx:
+                    subsetX.append(self.vecX[idx])
+                    subsetY.append(self.vecY[idx])
+                reg_model = Regression()
+                reg_model.vecX = np.array(subsetX)
+                reg_model.vecY = np.array(subsetY)
+                # Least Square Regression
+                theta_ = reg_model.leastSquaresReg()
+                predicY = reg_model.predict(theta_, self.polyx)
+                err, _ = reg_model.countErr(predicY, self.polyy)
+                temErr['LS'].append(err)
+                # Robust Regression
+                theta_ = reg_model.robustReg()
+                predicY = reg_model.predict(theta_, self.polyx)
+                err, _ = reg_model.countErr(predicY, self.polyy)
+                temErr['RR'].append(err)
+                # Regularized Least Square Regression
+                theta_ = reg_model.regularizedLSReg()
+                predicY = reg_model.predict(theta_, self.polyx)
+                err, _ = reg_model.countErr(predicY, self.polyy)
+                temErr['RLS'].append(err)
+                # Lasso
+                theta_ = reg_model.lassoReg()
+                predicY = reg_model.predict(theta_, self.polyx)
+                err, _ = reg_model.countErr(predicY, self.polyy)
+                temErr['Lasso'].append(err)
+
+            error['LS'].append(np.mean(np.array(temErr['LS'])))
+            error['RR'].append(np.mean(np.array(temErr['RR'])))
+            error['RLS'].append(np.mean(np.array(temErr['RLS'])))
+            error['Lasso'].append(np.mean(np.array(temErr['Lasso'])))
+
+        self.plotError(errorX, error['LS'], "LS")
+        self.plotError(errorX, error['RR'], "RR")
+        self.plotError(errorX, error['RLS'], "RLS")
+        self.plotError(errorX, error['RLS'], "RLS")
 
 
+    def plotError(self, inputX, inputY, name):
+        print inputX
+        print inputY
+        fig = plt.figure()
+        ax = fig.add_subplot(111)
+        ax.scatter(inputX, inputY, s=8, c="red")
+        ax.ylabel("Mean Square Error")
+        ax.xlabel("Size of Subset")
+        ax.title("Least Square Regression")
+        ax.plot(inputX, inputY)
+        ax.legend()
+        fig.savefig()
+        # ax.show()
 
         return 0
 
+    def run(self):
+        reg = Regression()
+        reg.genSubSetError()
 
+
+"""
 reg = Regression()
 reg.leastSquaresReg()  # least_squares(LS)f
 reg.regularizedLSReg()  # regularized LS (RLS)
 reg.robustReg()   # Robust Regression (RR)
 reg.baysesianReg() # Baysesian Regression (BR)
 reg.lassoReg()   # Lasso Regression (Lasso)
-
 plt.show()
-
+"""
+# reg = Regression()
+# reg.baysesianReg() # Baysesian Regression (BR)
